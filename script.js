e751de8cca0d3b6730a085f8db780ff4b2c8118f
Gefen <gefenbar009@gmail.com>
2022-08-02 17:37:04 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+prices = { "שניצל": 25, "צ'יפס": 15, "המבורגר": 20, "קולה": 10 }
 initApp()
 function initApp() {
     shopping_list = []
@@ -7,21 +8,21 @@ function initApp() {
 function mark(tile) {
     if (tile.textContent === '-') {
         tile.textContent = "V"
+        document.getElementById(tile.id).style.color = "#008000b3";
         shopping_list.push(tile.name)
     }
     else {
         tile.textContent = '-'
+        document.getElementById(tile.id).style.color = "transparent";
         shopping_list.pop(tile.name)
     }
 }
 
 function order() {
     for (let index = 0; index < shopping_list.length; index++) {
-        console.log(shopping_list[index])
+        product = shopping_list[index]
+        console.log(product + ":" + prices[product])
     }
 }
-function windowClose() {
-    window.open('','_parent','');
-    window.close();
-    }
+
 
