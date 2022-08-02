3dbe4667a9150ec4b01eed3589b647486e171aa0
Gefen <gefenbar009@gmail.com>
2022-08-02 13:07:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const winCombinations = [['0', '1', '2'], ['0', '3', '6'], ['3', '4', '5'], ['6', '7', '8'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']]
+initApp()
+function initApp() {
+    shopping_list = []
+    get_out = false
+}
+
+function mark(tile) {
+    if (tile.textContent === '-') {
+        tile.textContent = "V"
+        shopping_list.push(tile.name)
+    }
+    else {
+        tile.textContent = '-'
+        shopping_list.pop(tile.name)
+    }
+    console.log(shopping_list)
+}
+
+function navigation() {
+
+}
